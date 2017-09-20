ff7fc3f59d7f3df5c15b6a02f65b901a641d368a
PeitHegn <ad.mogensen@gmail.com>
2017-09-20 03:07:30 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# StoreSystemer
-School
+# choir-frontend
+
+See also:
+* [choir-contract](../../../choir-contract)
+* [choir-backend](../../../choir-backend)
+* [choir-backend-mock](../../../choir-backend-mock)
---
+++ src/main/java/dk/cphbusiness/choir/frontend/ChoirManagerController.java
@@ -0,0 +1,44 @@
+package dk.cphbusiness.choir.frontend;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import javax.ejb.EJB;
+
+@WebServlet(name = "ChoirManagerController", urlPatterns = {"/ChoirManager"})
+public class ChoirManagerController extends HttpServlet {
+
+  @EJB
+  //private ChoirManager manager;
+  
+//  private ChoirManager manager = new ChoirManagerDummy();
+
+  
+  
+  
+  @Override
+  protected void service(
+      HttpServletRequest request, // this is an interface - mock that
+      HttpServletResponse response // this is an interface - mock that
+      ) throws ServletException, IOException {
+    PrintWriter out = response.getWriter();
+    try {
+//      Collection<MemberSummary> members = manager.listMembers();
+//      request.setAttribute("members", members);
+      request.getRequestDispatcher("member-list.jsp").forward(request, response);
+      }
+    catch (Exception e) {
+      out.println("<h2>"+e.getMessage()+"</h2>");
+      out.print("<hr/><pre>");
+      e.printStackTrace(out);
+      out.println("</pre>");
+      }
+    }
+  
+  }
